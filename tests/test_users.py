e2897beb5c189e6df61dd9115fb0ea047e46e219
Yeldos <eldos_123@mail.ru>
2025-07-15 23:39:07 +0500

    параметризация Pytest. практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,10 @@
+import pytest
 from http import HTTPStatus
 
-import pytest
-
-from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
-from fixtures.users import UserFixture
+from tools.fakers import fake
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
@@ -13,16 +12,23 @@ from tools.assertions.users import assert_create_user_response, assert_get_user_
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
-    # Удалили инициализацию API клиента из теста
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize("email", [
+    fake.email(domain="mail.ru"),
+    fake.email(domain="gmail.com"),
+    fake.email(domain="example.com")
+])
+def test_create_user(email: str, public_users_client: PublicUsersClient):
+    """
+    Тест проверяет создание пользователя с email в указанном домене.
+    """
+    request = CreateUserRequestSchema(email=email)
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
-
     validate_json_schema(response.json(), response_data.model_json_schema())
+
 
 
 @pytest.mark.users

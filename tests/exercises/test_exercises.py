62632ee5ea621a16019eba6f06b010756a6546c0
Yeldos <eldos_123@mail.ru>
2025-07-17 23:44:41 +0500

    9.4 практикуемся в написании API автотестов. часть 3. практика написания API автотестов для работы с заданиями. Создание задания

---
+++ tests/exercises/test_exercises.py
@@ -0,0 +1,41 @@
+import pytest
+from http import HTTPStatus
+
+from clients.exercises.exercises_client import ExercisesClient
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from fixtures.courses import CourseFixture
+from tools.assertions.base import assert_status_code
+from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.schema import validate_json_schema
+
+
+@pytest.mark.exercises
+@pytest.mark.regression
+class TestExercises:
+    def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
+        """
+        Тест проверяет создание задания через API.
+
+        :param exercises_client: Экземпляр ExercisesClient для выполнения API-запросов
+        :param function_course: Фикстура, предоставляющая данные курса
+        :raises AssertionError: Если код ответа, тело ответа или JSON-схема не соответствуют ожидаемым
+        """
+        # Формируем данные для создания задания, используя схему CreateExerciseRequestSchema
+        request = CreateExerciseRequestSchema(
+            course_id=function_course.response.course.id,
+        )
+
+        # Отправляем запрос на создание задания
+        response = exercises_client.create_exercise_api(request)
+
+        # Преобразуем JSON-ответ в объект схемы CreateExerciseResponseSchema
+        response_data = CreateExerciseResponseSchema.model_validate_json(response.text)
+
+        # Проверяем статус-код ответа
+        assert_status_code(response.status_code, HTTPStatus.OK)
+
+        # Проверяем, что данные в ответе соответствуют запросу
+        assert_create_exercise_response(request, response_data)
+
+        # Валидируем JSON-схему ответа
+        validate_json_schema(response.json(), response_data.model_json_schema())
